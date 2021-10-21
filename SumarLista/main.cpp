4e82f6a59cdbb17fa7f6c84189efb2e81ffcf912
cbotella <carlosb.n@icloud.com>
2021-10-21 11:13:34 +0200

    SumarLista 1.0

--- SumarLista/main.cpp
+++ SumarLista/main.cpp
@@ -4,11 +4,29 @@
 //
 //  Created by Carlos Botella Navarro on 21/10/21.
 //
+/*
+--------------------------------------------------
+ Lista <R> --> sumarLista() --> R
+--------------------------------------------------
+*/
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int sumarLista(int *p, int cuantos){
+    int suma = 0;
+    for (int i = 0; i < cuantos; i++) {
+        cout << p[i] << endl;
+        suma = suma + p[i];
+        cout << "suma: " << suma << endl;
+    }
+    return suma;
+}
+
+int main(){
+    int lista[100]= {3,4,7,9,10};
+    int resultado = sumarLista(&lista[0],5);
+    cout << resultado;
+    
+    
 }
